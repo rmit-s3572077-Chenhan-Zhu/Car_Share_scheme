c65caa5c834d9675761c0a1a7fe8d848d096ef2c
rmit-s3572077-Chenhan-Zhu <s3572077@student.rmit.edu.au>
2018-05-16 22:34:53 +1000

    Add files via upload

---
+++ test_index.py
@@ -0,0 +1,24 @@
+import unittest
+from zhiliao import login,app
+import zhiliao
+import flask
+
+class Testmain(unittest.TestCase):
+
+
+    def setUp(self):
+        self.app = app.test_client()
+        self.app.testing = True
+
+    def tearDown(self):
+        pass
+
+ #ensure that flask was set up correctly, the reason of returning 302 is redirect(if no login).
+    def test_index_status_code(self):
+        result = self.app.get('/')
+        self.assertEqual(result.status_code, 302)
+
+
+
+if __name__ =='__main__':
+    unittest.main()
--- test_login.py
+++ test_login.py
@@ -1,8 +1,9 @@
 import unittest
 from zhiliao import login,app
 import zhiliao
+import flask
 
-class Testzhiliao(unittest.TestCase):
+class Testmain(unittest.TestCase):
 
 
     def setUp(self):
@@ -12,10 +13,17 @@ class Testzhiliao(unittest.TestCase):
     def tearDown(self):
         pass
 
+ #test return infomation if username or password are invalid
+    def test_incorrect_login(self):
+        result = self.app.post('/login/', data=dict(username="jingming", password="1234567890" ), follow_redirects=True)
+        self.assertIn('wrong username or password,please try again',result.data)
 
-    def test_login(self):
-        result = self.app.get('/')
-        self.assertEqual(result.status_code, 302)
+ #test login status when login successful
+    def test_correct_login(self):
+        result = self.app.post('/login/', data=dict(username="jingming", password="123"),
+                               follow_redirects=True)
+        self.assertEqual(result.status_code, 200)
+
 
 if __name__ =='__main__':
     unittest.main()
